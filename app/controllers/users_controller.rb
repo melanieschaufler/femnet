de2ae8f5f73503e674b95b3fc61906a53826ae55
Vincent Michler vincent.michler94@gmail.com <vincent.michler94@gmail.com>
2020-08-11 12:10:41 +0200

    Routes und Edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.where(mentor: true)
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      # Sign in the user by passing validation in case their password changed
+      # bypass_sign_in(@user)
+      # redirect_to show_user_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    # NOTE: Using `strong_parameters` gem
+    params.require(:user).permit(:name, :profession)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :profiles, only: [:show, :edit, :update, :destroy]
 
-  ## Einbau search in index for mentors
+  resources :users
+  #   collection do
+  #     get :mentors
+  #   end
+  # end   ## Einbau search in index for mentors
 
 
 end
