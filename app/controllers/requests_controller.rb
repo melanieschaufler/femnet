d2b316284d731428f34ce419d81f8093da1cdf10
Louisa Scheiter <22.leo.scheiter@gmail.com>
2020-08-17 15:09:35 +0200

    Last messages of the other person in the dashboard

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,6 +5,16 @@ class RequestsController < ApplicationController
   def my_requests
     @my_requests = current_user.requests_as_receiver
     authorize @my_requests
+
+    @user = current_user
+    if current_user.mentor
+      @my_requests = current_user.requests_as_receiver.where(status: "Accepted")
+    else
+      @my_requests = current_user.requests_as_asker.where(status: "Accepted")
+    end
+    @my_messages = @my_requests.map do |request|
+      request.chatroom.messages.last
+    end
   end
 
   def show
