5e196b704fe29a37eeec8be11ed1fea000aba378
Vincent Michler vincent.michler94@gmail.com <vincent.michler94@gmail.com>
2020-08-18 11:57:05 +0200

    Inserted mapbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_132831) do
+ActiveRecord::Schema.define(version: 2020_08_18_080339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,8 @@ ActiveRecord::Schema.define(version: 2020_08_13_132831) do
     t.text "answer2"
     t.text "answer3"
     t.boolean "mentor"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
